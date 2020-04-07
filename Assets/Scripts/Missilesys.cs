15ef4122e9721ffd60fad0f6ed9466cb309fadf0
Enterpryz <enterpryz1@gmail.com>
2020-04-07 16:57:32 -0500

    Make projectile move

---
+++ Assets/Scripts/Missilesys.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missilesys : MonoBehaviour
+{
+
+    [SerializeField] float speed = 1f; //varmisspeed
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime); //varmisspeed
+    }
+}
