6df838fadb213b927a0a068d39883e8f7b305a23
Enterpryz <enterpryz1@gmail.com>
2020-04-07 16:06:51 -0500

    Instantiate from gun position

---
+++ Assets/Scripts/Actionscript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actionscript : MonoBehaviour
+{
+
+    [SerializeField] GameObject projectile, bullet; //GOprojectiledef
+    
+
+    public void Fire() //PubvoidFire(float vardamage)
+    {
+        Instantiate(projectile, bullet.transform.position, transform.rotation);
+    }
+
+
+}
