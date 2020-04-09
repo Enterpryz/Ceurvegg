cdb5d97ca57be326bf7ecb9ddd5dd8b777b80398
Enterpryz <enterpryz1@gmail.com>
2020-04-09 16:27:14 -0400

    Tidy Up and Death FX, Click To Spawn Defenders, Spawn At Mouse Position (Forgot to commit earleir)

--- Assets/Scripts/DMGsys.cs
+++ Assets/Scripts/DMGsys.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class DMGsys : MonoBehaviour
 {
     [SerializeField] float flthealth = 100f;
-
+    [SerializeField] GameObject deathVFX;
+    
     public void reducehp(float fltdamage)
     {
         flthealth -= fltdamage;
         if (flthealth <= 0)
         {
+            TriggerDeathVFX();
             Destroy(gameObject);
         }
     }
+
+    private void TriggerDeathVFX()
+    {
+        if(!deathVFX) { return; }
+        GameObject deathVFXObkect = Instantiate(deathVFX, transform.position, transform.rotation);
+    }
+
+
+
+
 
 }
---
+++ Assets/Scripts/Defenderspawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defenderspawn : MonoBehaviour
+{
+    [SerializeField] GameObject GOdefender;
+    
+    private void OnMouseDown()
+    {
+        DeployDef(GetSquareClicked());
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+    }
+
+
+
+    private void DeployDef(Vector2 worldPos)
+    {
+        GameObject GODefnew = Instantiate(GOdefender, worldPos, Quaternion.identity) as GameObject;
+    }
+
+
+
+
+
+
+}
