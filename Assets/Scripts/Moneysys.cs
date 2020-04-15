f74153a6edd493be363177eddac78b239cdc9a57
Enterpryz <enterpryz1@gmail.com>
2020-04-15 18:40:05 -0400

    Start Our Resource System

---
+++ Assets/Scripts/Moneysys.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Moneysys : MonoBehaviour
+{
+    [SerializeField] int intMoney = 100;
+    Text Mohneetext;
+
+
+    void Start()
+    {
+        Mohneetext = GetComponent <Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        Mohneetext.text = intMoney.ToString();
+    }
+
+    public void AddMoney(int amount)
+    {
+        intMoney += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendMoney(int amount)
+    {
+        if (intMoney >= amount)
+        {
+            intMoney -= amount;
+            UpdateDisplay();
+        }
+    }
+
+
+
+
+}
