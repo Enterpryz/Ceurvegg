664b4f8ba60cbfd3d734cd83d2c269d8c6e7d1eb
Enterpryz <enterpryz1@gmail.com>
2020-04-19 19:19:51 -0400

    Attacker In My Lane

--- Assets/Scripts/Actionscript.cs
+++ Assets/Scripts/Actionscript.cs
@@ -7,7 +7,60 @@ public class Actionscript : MonoBehaviour
 
     [SerializeField] GameObject bullet;
     [SerializeField] GameObject projectile;
+    AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+        
     
+    
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("BANG POW POP WOLLOP");
+        }
+        else
+        {
+            Debug.Log("Vibe time");
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough =
+               (Mathf.Abs(spawner.transform.position.y - transform.position.y)
+               <= Mathf.Epsilon);
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+        
+    }
+       
+
+
     public void Fire() 
     {
         Instantiate(projectile, bullet.transform.position, transform.rotation);
